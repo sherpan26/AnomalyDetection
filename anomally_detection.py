78df1e49bc9943dbe6afedd9ab1be00bb27f6599
sherpan26 <145368455+sherpan26@users.noreply.github.com>
2025-06-10 10:23:47 -0400

    anomally_detection.py

--- anomally_detection.py
+++ anomally_detection.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
@@ -13,7 +14,29 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 # K-Means clustering (k=2)
-kmeans = KMeans(n_clusters=2, random_state=42)
+#manually select example anomaly and normal samples
+# tEMP anomaly: pick a row where temp is highest
+temp_idx = X["Temperature_C"].idxmax()
+temp_anomaly = X_scaled[temp_idx]
+# EC short anomaly: pick highest EC
+ec_idx = X["EC_dS_m"].idxmax()
+ec_anomaly = X_scaled[ec_idx]
+# combine anomalies
+anomaly_init = np.mean([temp_anomaly, ec_anomaly], axis=0)
+
+# normal centroid take the average of all points
+normal_init = np.mean(X_scaled, axis=0)
+init_centroids = np.vstack([normal_init, anomaly_init])
+
+kmeans = KMeans(
+    n_clusters=2,
+    init=init_centroids,
+    n_init=1,                # Because we’re giving explicit centroids
+    max_iter=500,            # More iterations
+    tol=1e-5,                # Lower tolerance for convergence
+    random_state=42
+)
+
 labels = kmeans.fit_predict(X_scaled)
 
 # decide which cluster is anomaly:
@@ -52,3 +75,4 @@ plt.boxplot(X_scaled, labels=X.columns, vert=False)
 plt.title("Feature Distributions (Z-score Normalized)")
 plt.tight_layout()
 plt.show()
+
